5fcb1c39cc5434b07119cb3002064406e36c9248
allhailjustice <42614647+allhailjustice@users.noreply.github.com>
2018-11-21 16:20:08 -0600

    Add files via upload

--- test.py
+++ test.py
@@ -2,17 +2,30 @@ from graphlet import *
 from layout import draw
 from knn import knn
 
+# func create_matrix: input are (int(number of vertices),int(number of edges)), output is nd-array(matrix)
+# func RW: input is nd-array(matrix), output is 1d-array(graphlet frequency)
+# func knn: input are (nd-array(train graphlet frequencies), 1d-array(test graphlet frequency)),
+#           output is the nearest neighbour's idx
+# function draw: input are (nd-array(matrix),str(figure name))
+
+
 features = []
 for i in range(1000):
+    # create a matrix
     matrix = create_matrix(100, 100)
+
+    # calculate graphlet frequency
     features.append(RW(matrix))
     np.save('matrices/synthetic'+str(i), matrix)
     print(i)
-
 np.save('train_features', features)
-features = np.load('train_features.npy')
+
+# create test matrix
 test_matrix = create_matrix(100, 100)
+
+# train knn model
 idx = knn(features, RW(test_matrix))
-print(idx)
+
+# show and save the real and estimated layouts
 draw(np.load('matrices/synthetic'+str(idx)+'.npy'), 'estimated_layout')
 draw(test_matrix, 'real_layout')
