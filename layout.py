6197ad61f7db29cfc5552bfad9cd34698f78aab5
allhailjustice <42614647+allhailjustice@users.noreply.github.com>
2018-12-13 15:12:37 -0600

    Update layout.py

--- layout.py
+++ layout.py
@@ -33,3 +33,18 @@ def draw2(matrix, name, close, axes):
     plt.savefig(name)
     if(close):
         plt.close()
+
+def draw3(matrix, name, close, axes):
+    A, B = np.where(matrix == 1)
+    A = list(A)
+    B = list(B)
+    df = pd.DataFrame({'from': A, 'to': B})
+    G = nx.from_pandas_edgelist(df, source='from', target='to')
+    if(axes==None):
+        nx.draw(G, with_labels=False, node_size=50, node_color="skyblue", ax=axes, pos=nx.spectral_layout(G))
+    else:
+        nx.draw(G, with_labels=False, node_size=50, node_color="skyblue", ax=axes, pos=nx.spectral_layout(G))
+    plt.title("Spectral method")
+    plt.savefig(name)
+    if(close):
+        plt.close()        
